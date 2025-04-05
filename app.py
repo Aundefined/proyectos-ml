2805d95ee2c1bdd620f89c169cf388143001d331
Arnaud Tartulier <tartulierarnaud@gmail.com>
2025-04-05 21:25:02 +0200

    ajustes

--- app.py
+++ app.py
@@ -121,6 +121,7 @@ def predictor_sueldos():
                                resultado=resultado, 
                                error=error, 
                                form_data=form_data)
-
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
